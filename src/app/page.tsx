af4fa75d2bfa9a3755a07884f6158ed045b6301d
vsshetty18 <vsvighnesh2026@gmail.com>
2026-05-24 21:32:12 +0530

    Create HomePage component for main application

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+export default function HomePage() {
+
+  return (
+    <div style={{ padding: 40 }}>
+
+      <h1>
+        Prowider Lead System
+      </h1>
+
+      <a href="/request-service">
+        Request Service
+      </a>
+
+    </div>
+  );
+
+}
